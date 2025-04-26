ebdffad6ce4c5c46a52a51d664aa55574089f3c1
Rawezh0894 <rawezh.soft94@gmail.com>
2025-04-26 10:37:51 +0300

    commit

--- update.sh
+++ update.sh
@@ -3,11 +3,14 @@
 # Navigate to the project directory
 cd /var/www/html/warehoause-system
 
+# Configure Git to use rebase strategy
+git config pull.rebase true
+
 # Save any local changes
 git stash
 
-# Pull the latest changes
-git pull origin main
+# Pull the latest changes with rebase
+git pull --rebase origin main
 
 # Apply any saved changes
 git stash pop
