49abde2b863b63dd37cdc5eb92d49ea35c61afe2
Rawezh0894 <rawezh.soft94@gmail.com>
2025-04-26 14:18:46 +0300

    commit

--- Selling-System/src/api/receipts/filter_sales.php
+++ Selling-System/src/api/receipts/filter_sales.php
@@ -1,7 +1,7 @@
 <?php
 header('Content-Type: application/json');
-require_once '../../config/database.php';
-require_once '../../controllers/receipts/SaleReceiptsController.php';
+require_once __DIR__ . '/../../config/database.php';
+require_once __DIR__ . '/../../Controllers/receipts/SaleReceiptsController.php';
 
 try {
     // Set up filters based on request parameters
