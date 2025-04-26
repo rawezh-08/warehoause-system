9436d406711e1d9381698bf5727d3a031b3466b7
Rawezh0894 <rawezh.soft94@gmail.com>
2025-04-26 18:02:45 +0300

    commit

--- Selling-System/src/js/addReceipt.js
+++ Selling-System/src/js/addReceipt.js
@@ -611,7 +611,7 @@ $(document).ready(function() {
             
             if (receiptId) {
                 // Open print window for the saved receipt
-                window.open(`../../Selling-System/src/views/admin/receipt/print_receipt.php?sale_id=${receiptId}`, '_blank');
+                window.open(`/Selling-System/src/views/receipt/print_receipt.php?sale_id=${receiptId}`, '_blank');
             } else {
                 Swal.fire({
                     title: 'پسوڵە پاشەکەوت نەکراوە',
@@ -1040,7 +1040,7 @@ $(document).ready(function() {
             }).then((result) => {
                 if (result.isConfirmed) {
                     // Open print window
-                    window.open(`/Selling-System/src/views/admin/receipt/print_receipt.php?sale_id=${receiptId}`, '_blank');
+                    window.open(`/Selling-System/src/views/receipt/print_receipt.php?sale_id=${receiptId}`, '_blank');
                 }
                 
                 // Reset form after handling print dialog
