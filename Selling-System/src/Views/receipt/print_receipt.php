e84d200773dc88abd7fb82a705da41aa9f517bd9
Rawezh0894 <rawezh.soft94@gmail.com>
2025-04-27 15:03:19 +0300

    commit

--- Selling-System/src/Views/receipt/print_receipt.php
+++ Selling-System/src/Views/receipt/print_receipt.php
@@ -673,8 +673,8 @@ $dir = $lang === 'ar' ? 'rtl' : 'rtl';
                 <div class="company-info">
                     <h1><?php echo $t['company_name']; ?></h1>
                     <p><?php echo $t['address']; ?></p>
-                    <p style="direction: ltr; text-align: start;"><i class="fas fa-phone"></i> 0771 225 5656</p>
-                    <p style="direction: ltr; text-align: start;"><i class="fas fa-phone"></i> 0750 147 8786</p>
+                    <p>0771 225 5656</p>
+                    <p>0750 147 8786</p>
                 </div>
             </div>
             <div class="invoice-details">
