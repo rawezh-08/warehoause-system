7e3b3a47ad266afca32f81639a2e9c312972a3c6
Rawezh0894 <rawezh.soft94@gmail.com>
2025-04-28 12:07:03 +0300

    Update: Your commit message here

--- Selling-System/src/components/sidebar.php
+++ Selling-System/src/components/sidebar.php
@@ -11,7 +11,7 @@
                 <div class="dash-cont">
                     <img src="../../assets/icons/dashboard.svg" alt="" class="dash-icon">
                 </div>
-                <span>پەیوەندییەکان</span>
+                <span>ەیوەندییەکان</span>
             </a>
         </div>
 
