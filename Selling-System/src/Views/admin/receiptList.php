d70c1f710091334f460762afbd73648444a21242
Rawezh0894 <rawezh.soft94@gmail.com>
2025-04-30 22:39:19 +0300

    گۆڕانکاریە نوێیەکان

--- Selling-System/src/Views/admin/receiptList.php
+++ Selling-System/src/Views/admin/receiptList.php
@@ -342,6 +342,7 @@ function formatDate($date) {
     <!-- SweetAlert2 JS -->
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.7.32/dist/sweetalert2.all.min.js"></script>
     <!-- Page Script -->
+    <script src="../../js/include-components.js"></script>
     <script src="../../js/receiptList.js"></script>
 </body>
 
