9a59f935d789f2cf7004ab8fa0a01c8d2acc9802
Rawezh0894 <rawezh.soft94@gmail.com>
2025-05-01 19:37:53 +0300

    گۆڕانکاریە نوێیەکان

--- Selling-System/src/Views/admin/addReceipt.php
+++ Selling-System/src/Views/admin/addReceipt.php
@@ -233,9 +233,6 @@ require_once '../../config/database.php';
                         <button type="button" class="btn btn-outline-success delivery-btn ms-2">
                             <i class="fas fa-truck"></i> پسووڵەی گەیاندن
                         </button>
-                        <button type="button" class="btn btn-info price-profit-btn ms-2">
-                            <i class="fas fa-calculator"></i> نرخ و قازانج
-                        </button>
                         <button type="button" class="btn btn-primary save-btn cta-btn ms-2">
                             پاشەکەوتکردن <i class="fas fa-save"></i>
                         </button>
